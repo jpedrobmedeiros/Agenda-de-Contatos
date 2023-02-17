eb12f4f0a3b86e4741181945d66c6c6ed8d38512
João Pedro Barbosa Medeiros <jpedrobmedeiros@gmail.com>
2023-02-17 16:17:32 -0500

    Adição do arquivo erro.py

---
+++ erro.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# Erro em tempo de compilação
+# SyntaxError
+while True
+    print("Olá Mundo!")
+
+# Erro em tempo de execução
+# ZeroDivisionError
+print(2 / 0)
+
+# Erro de lógica
+#função exercendo uma operação diferente do seu propósito
+def dividir(a, b):
+    print(a * b)
+
+dividir(2, 2)
