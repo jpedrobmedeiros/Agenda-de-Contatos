8424044c89756dd50ecd48537a432dc8c8981f69
João Pedro Barbosa Medeiros <jpedrobmedeiros@gmail.com>
2023-02-17 12:43:00 -0500

    Ajustes na aparência do programa e tornando o menu repetitivo

--- agenda.py
+++ agenda.py
@@ -17,13 +17,14 @@ AGENDA['João'] = {
 def mostrarAgenda():
     for contato in AGENDA:
         buscarContato(contato)
-        print("-----------------------------")
 
 def buscarContato(contato):
+    print("-------------------------------------")
     print("Nome:", contato)
     print("Telefone:", AGENDA[contato]["telefone"])
     print("E-mail:", AGENDA[contato]["email"])
     print("Endereço:", AGENDA[contato]["endereco"])
+    print("-------------------------------------")
 
 def incluirEditarContato(contato, telefone, email, endereco):
     AGENDA[contato] = {
@@ -31,39 +32,50 @@ def incluirEditarContato(contato, telefone, email, endereco):
         "email": email,
         "endereco": endereco
     }
+    print("-------------------------------------")
     print(f">>>>> Contato {contato} adicionado/editado com sucesso!")
+    print("-------------------------------------")
 
 def excluirContato(contato):
     AGENDA.pop(contato)
+    print("-------------------------------------")
     print(f">>>>> Contato {contato} excluido com sucesso!")
+    print("-------------------------------------")
 
 def imprimirMenu():
+    print("-------------------------------------")
     print("1 - Mostrar Agenda")
     print("2 - Buscar Contato")
     print("3 - Incluir Contato")
     print("4 - Editar Contato")
     print("5 - Excluir Contato")
     print("0 - Fechar Programa")
+    print("-------------------------------------")
 
-imprimirMenu()
-OPCAO = input("Digite o número da opção: ")
+while True:
+    imprimirMenu()
+    OPCAO = input("Digite o número da opção: ")
 
-if OPCAO == "1":
-    mostrarAgenda()
-elif OPCAO == "2":
-    contato = input("Digite o nome do contato: ")
-    buscarContato(contato)
-elif OPCAO == "3" or OPCAO == "4":
-    contato = input("Digite o nome do contato: ")
-    telefone = input("Digite o telefone do contato: ")
-    email = input("Digite o e-mail do contato: ")
-    endereco = input("Digite o endereco do contato: ")
-    incluirEditarContato(contato, telefone, email, endereco)
-elif OPCAO == "5":
-    contato = input("Digite o nome do contato: ")
-    excluirContato(contato)
-elif OPCAO == "0":
-    print("Fechando o programa...")
-    exit()
-else:
-    print("Opção inválida!")
+    if OPCAO == "1":
+        mostrarAgenda()
+    elif OPCAO == "2":
+        contato = input("Digite o nome do contato: ")
+        buscarContato(contato)
+    elif OPCAO == "3" or OPCAO == "4":
+        contato = input("Digite o nome do contato: ")
+        telefone = input("Digite o telefone do contato: ")
+        email = input("Digite o e-mail do contato: ")
+        endereco = input("Digite o endereco do contato: ")
+        incluirEditarContato(contato, telefone, email, endereco)
+    elif OPCAO == "5":
+        contato = input("Digite o nome do contato: ")
+        excluirContato(contato)
+    elif OPCAO == "0":
+        print("-------------------------------------")
+        print(">>>>> Fechando o programa...")
+        print("-------------------------------------")
+        exit()
+    else:
+        print("-------------------------------------")
+        print(">>>>> Opção inválida!")
+        print("-------------------------------------")
