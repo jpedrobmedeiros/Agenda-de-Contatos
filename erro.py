18a2f8836d15c9deeafd594bdfbe990398a3f6d8
João Pedro Barbosa Medeiros <jpedrobmedeiros@gmail.com>
2023-02-17 16:36:57 -0500

    Adicionando exemplo de try/except em erro.py

--- erro.py
+++ erro.py
@@ -2,16 +2,25 @@
 
 # Erro em tempo de compilação
 # SyntaxError
-while True
-    print("Olá Mundo!")
+# while True
+#     print("Olá Mundo!")
 
 # Erro em tempo de execução
 # ZeroDivisionError
-print(2 / 0)
+# print(2 / 0)
 
 # Erro de lógica
 #função exercendo uma operação diferente do seu propósito
-def dividir(a, b):
-    print(a * b)
+#def dividir(a, b):
+#    print(a * b)
 
-dividir(2, 2)
+#dividir(2, 2)
+
+def dividir(a, b):
+    try:
+        print(a / b)
+    except Exception as e:
+        print("Divisão inválida!")
+        print(e)
+
+dividir(2, "a")
