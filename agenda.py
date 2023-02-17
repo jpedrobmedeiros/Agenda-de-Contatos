030bebc8abfb389b4fec86180c925efb43f80977
João Pedro Barbosa Medeiros <jpedrobmedeiros@gmail.com>
2023-02-17 11:02:47 -0500

    Estrutura base do menu criada

--- agenda.py
+++ agenda.py
@@ -37,9 +37,33 @@ def excluirContato(contato):
     AGENDA.pop(contato)
     print(f">>>>> Contato {contato} excluido com sucesso!")
 
-mostrarAgenda()
-incluirEditarContato("Paulo", "93333-3333", "paulo@email.com", "Av. 3")
-incluirEditarContato("João", "93333-4444", "jp@email.com", "Av. 2")
-incluirEditarContato("José", None, None, "Av. 4")
-excluirContato("Maria")
-mostrarAgenda()
+def imprimirMenu():
+    print("1 - Mostrar Agenda")
+    print("2 - Buscar Contato")
+    print("3 - Incluir Contato")
+    print("4 - Editar Contato")
+    print("5 - Excluir Contato")
+    print("0 - Fechar Programa")
+
+imprimirMenu()
+OPCAO = input("Digite o número da opção: ")
+
+if OPCAO == "1":
+    mostrarAgenda()
+elif OPCAO == "2":
+    contato = input("Digite o nome do contato: ")
+    buscarContato(contato)
+elif OPCAO == "3" or OPCAO == "4":
+    contato = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    email = input("Digite o e-mail do contato: ")
+    endereco = input("Digite o endereco do contato: ")
+    incluirEditarContato(contato, telefone, email, endereco)
+elif OPCAO == "5":
+    contato = input("Digite o nome do contato: ")
+    excluirContato(contato)
+elif OPCAO == "0":
+    print("Fechando o programa...")
+    exit()
+else:
+    print("Opção inválida!")
